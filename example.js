5dc5d24fe78309946a84e430839d83340c20f977
Nishant <sudoaccessdenied@gmail.com>
2023-08-20 03:21:45 +0530

    side reduced

--- example.js
+++ example.js
@@ -40,7 +40,7 @@ function onLocalTracks(tracks) {
                 console.log(
                     `track audio output device was changed to ${deviceId}`));
         if (localTracks[i].getType() === 'video') {
-            $('body').append(`<video autoplay='1' id='localVideo${i}' />`);
+            $('body').append(`<video autoplay='1' id='localVideo${i}' width="400" height="400" />`);
             localTracks[i].attach($(`#localVideo${i}`)[0]);
         } else {
             $('body').append(
